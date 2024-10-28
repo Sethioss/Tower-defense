e375598d83c2f6e04df77f824c90821cb13f5f33
Sethioss <sebastien.guarober@gmail.com>
2024-10-28 23:00:06 +0100

    Added custom log categories for the LightweightAbilitySystem module

---
+++ Source/LightweightAbilitySystem/Private/AbilitySystem.cpp
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AbilitySystem.h"
+
+DEFINE_LOG_CATEGORY(AbilitySystemLog);
+DEFINE_LOG_CATEGORY(AbilitySystemWarning);
+DEFINE_LOG_CATEGORY(AbilitySystemError);
+
+// Sets default values for this component's properties
+UAbilitySystem::UAbilitySystem()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UAbilitySystem::BeginPlay()
+{
+	Super::BeginPlay();
+	UE_LOG(AbilitySystemLog, Log, TEXT("Ability system logging!"));
+	UE_LOG(AbilitySystemWarning, Warning, TEXT("Ability system warning!"));
+	UE_LOG(AbilitySystemError, Error, TEXT("Ability system error!"));
+
+	// ...
+	
+}
+
+
+// Called every frame
+void UAbilitySystem::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
---
+++ Source/LightweightAbilitySystem/Public/LightWeightAbilitySystemCore.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+LIGHTWEIGHTABILITYSYSTEM_API DECLARE_LOG_CATEGORY_EXTERN(AbilitySystemLog, Log, All);
+LIGHTWEIGHTABILITYSYSTEM_API DECLARE_LOG_CATEGORY_EXTERN(AbilitySystemWarning, Warning, All);
+LIGHTWEIGHTABILITYSYSTEM_API DECLARE_LOG_CATEGORY_EXTERN(AbilitySystemError, Error, All);
+
+/**
+ * 
+ */
+class LIGHTWEIGHTABILITYSYSTEM_API LightWeightAbilitySystemCore
+{
+public:
+	LightWeightAbilitySystemCore();
+	~LightWeightAbilitySystemCore();
+};
